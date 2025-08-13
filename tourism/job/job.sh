620ef641d2f0bfdb8c6bf5976a55073a66039a20
Vincent Su <datsu2003@gmail.com>
2025-08-13 21:10:35 +1000

    Update job.sh

--- tourism/job/job.sh
+++ tourism/job/job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=sim_chunk
 #SBATCH --output=tourism/job/logs/output_%A_%a.log
 #SBATCH --error=tourism/job/logs/error_%A_%a.log
-#SBATCH --array=1-18,20-133
+#SBATCH --array=1-133
 #SBATCH --time=24:00:00
 #SBATCH --mem=2G
 #SBATCH --cpus-per-task=1
