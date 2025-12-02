db08c1f7585c28201c106c117056ff3cf4186536
Vincent Su <datsu2003@gmail.com>
2025-12-02 16:51:08 +1100

    run NOVELIST PC std

--- tourism/PC_standardised_run.R
+++ tourism/PC_standardised_run.R
@@ -12,7 +12,6 @@ library(dplyr)
 library(ReconCov)
 
 
-
 visnights_full <- readRDS("tourism/data/visnights_full.rds")
 S <- readRDS("tourism/data/S.rds")
 row_names <- readRDS("tourism/data/row_names.rds")
@@ -42,54 +41,52 @@ run <- function(i) {
 
   remove(result) # free memory
 
-  # y <- y_full[i:(seq_dates$end[i] - start_date + 1), ]
-  # y_hat <- y - resid
+  y <- y_full[i:(seq_dates$end[i] - start_date + 1), ]
+  y_hat <- y - resid
   
-  Ks <- c(1, 2, 5, 10)
+  Ks <- c(1, 2, 5, 10, 15)
 
-  # shrinkage estimator with PC adjustment
-  W_shr_pc <- lapply(Ks, function(K) {
-    shrinkage_pc_est(resid, K = K)
-  })
-  names(W_shr_pc) <- paste0("K", Ks)
+  # # shrinkage estimator with PC adjustment
+  # W_shr_pc <- lapply(Ks, function(K) {
+  #   shrinkage_pc_est(resid, K = K)
+  # })
+  # names(W_shr_pc) <- paste0("K", Ks)
   
-  recon_mint_shr_pc <- lapply(W_shr_pc, function(W) {
-    reconcile_mint(base_fc, S, W$cov)
-  })
-
-  # window <- round(dim(y)[1] * 0.7)
-  # # NOVELIST estimator with PC adjustment
-  # W_n_list <- novelist_pc_cv(
-  #   y,
-  #   y_hat,
-  #   Ks = Ks,
-  #   S,
-  #   window = window,
-  #   deltas = seq(0, 1, by = 0.05),
-  #   standardise = TRUE,
-  #   ensure_PD = TRUE,
-  #   message = FALSE
-  # )
-
-  # recon_mint_n_pc <- lapply(W_n_list$cov, function(W) {
-  #   reconcile_mint(base_fc, S, W)
+  # recon_mint_shr_pc <- lapply(W_shr_pc, function(W) {
+  #   reconcile_mint(base_fc, S, W$cov)
   # })
 
-  e <- lapply(recon_mint_shr_pc, function(x) actual - x) # sublist
-  # e <- lapply(recon_mint_n_pc, function(x) actual - x) # sublist
-
-  # lambdas for shrinkage
-  W_shr_lambdas <- c(
-    sapply(W_shr_pc, function(x) x$lambda)
+  window <- round(dim(y)[1] * 0.7)
+  # NOVELIST estimator with PC adjustment
+  W_n_list <- novelist_pc_cv(
+    y, y_hat,
+    Ks = Ks, S,
+    window = window,
+    deltas = seq(0, 1, by = 0.05),
+    standardise = TRUE,
+    ensure_PD = TRUE,
+    message = FALSE
   )
+
+  recon_mint_n_pc <- lapply(W_n_list$cov, function(W) {
+    reconcile_mint(base_fc, S, W)
+  })
+
+  # e <- lapply(recon_mint_shr_pc, function(x) actual - x) # sublist
+  e <- lapply(recon_mint_n_pc, function(x) actual - x) # sublist
+
+  # # lambdas for shrinkage
+  # W_shr_lambdas <- c(
+  #   sapply(W_shr_pc, function(x) x$lambda)
+  # )
 
   return(list(
     e = e,
-    W_shr = W_shr_lambdas
-    # W_n = rbind(
-    #   delta = W_n_list$delta,
-    #   lambda = W_n_list$lambda
-    # )
+    # W_shr = W_shr_lambdas
+    W_n = rbind(
+      delta = W_n_list$delta,
+      lambda = W_n_list$lambda
+    )
   ))
 }
 
@@ -102,7 +99,7 @@ handlers("txtprogressbar")  # or "progress" for a fancier bar
 
 plan(multisession, workers = parallel::detectCores() - 1)
 
-M <- 97
+M <- 60
 # parallel with progress bar
 with_progress({
   p <- progressor(along = 1:M)  # auto sets steps = length
